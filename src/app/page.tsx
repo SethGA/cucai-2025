042249f9450f382f2839d1d15d6d01e29e8caf48
Kevin Wang <kayhne999@gmail.com>
2024-08-13 19:18:26 -0400

    Minor header work, set up template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <div>
+      {/* landing header */}
+      <div className='header'>
+        <Image 
+          src="/image.png" 
+          alt="CUCAI logo"
+          width={142.8}
+          height={68} 
+        />
+        
+      </div>
+
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-    </main>
+    </div>
   );
 }
