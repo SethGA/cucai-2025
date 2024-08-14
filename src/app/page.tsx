1bcde494b82157621bda0da7854156545d414ec4
Kevin Wang <kayhne999@gmail.com>
2024-08-13 23:22:19 -0400

    Adding mode toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,38 @@
 import Image from 'next/image'
 
+let buttons = [
+  {name: "About", link: "/"},
+  {name: "Speakers", link: "/"},
+  {name: "Schedule", link: "/"},
+  {name: "Sponsors", link: "/"},
+  {name: "Register", link: "/"},
+]
+
 export default function Home() {
   return (
     <div>
       {/* landing header */}
       <div className='header'>
-        <Image 
-          src="/image.png" 
-          alt="CUCAI logo"
-          width={142.8}
-          height={68} 
-        />
-        <div className='menuList'>
+        <div className='cucai-logo'>
+          <Image 
+            src="/image.png" 
+            alt="CUCAI logo"
+            width={142.8}
+            height={68} 
+          />
+        </div>
+        <div className='menu-list'>
+          {buttons.map((b) => (
+            <a href={b.link} className='menu-option' key={b.name}>{b.name}</a>
+            ))}
+          <a href="/" className="menu-toggle">
+            <Image 
+              src="/Mode Toggle.png" 
+              alt="Toggle Mode"
+              width={40}
+              height={40}
+            />
+          </a>
         </div>
       </div>
 
