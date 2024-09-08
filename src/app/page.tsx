4c9ceec885f2df6cbcb7d47144b6154ba0027bd4
Kevin Wang <kayhne999@gmail.com>
2024-09-08 14:39:54 -0400

    First draft of first page work
    Subject to changes once MVP is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Header from "./Header";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
 const [isDark, setDark] = useState<boolean>(true);
@@ -10,8 +11,45 @@ const [isDark, setDark] = useState<boolean>(true);
     <div>
       <Header isDark={isDark} setDark={setDark} />
       <div className='first-section'>
-        <h1 className="big-text">Join the Future of AI at CUCAI 2025</h1>
+        <div className="toronto-skyline">
+          <div className="white-box">
+            <h1 className="title">{"Experience AI\'s Future at CUCAI 2025!"}</h1>
+            <p className="subtext">Empowering the Next Generation of AI Innovators</p>
+            <a href="/" className="register-button">Register Now</a>
+          </div>
+        </div>
       </div>
+      <div className="second-section">
+        {/* 
+          Where Tomorrow's AI Innovators Meet
+          Video 
+          Apply Now
+        */}
+        <div className="web-video">
+          {/* <p>lalala Test Test Test</p> */}
+        </div>
+      </div>
+      <div className="third-section">
+        {/* 
+          AI Experts on Stage
+        */}
+      </div>
+      <div className="fourth-section">
+        {/* 
+          Our Student Partners
+        */}
+      </div>
+      <div className="fifth-section">
+        {/* 
+          Transform Tomorrow Together
+        */}
+      </div>
+      <div className="sixth-section">
+        {/* 
+          Leaders Behind the Vision
+        */}
+      </div>
+      
     </div>
   );
 }
