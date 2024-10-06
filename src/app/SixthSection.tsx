1c0714bf09316c6eea91031cda7b236581e30c85
Kevin Wang <kayhne999@gmail.com>
2024-10-06 19:13:04 -0400

    Minds behind CUCAI First draft
    No Bugs
    Need to add Staff

--- src/app/SixthSection.tsx
+++ src/app/SixthSection.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
 import { leaders } from "./page";
+import { IBM_Plex_Sans } from "next/font/google";
 
+// TODO: Replace with CUCAI 2025 Staff
 const Team = [
   {
     name: "Marcelo Chaman Mallqui",
@@ -83,18 +85,29 @@ const Team = [
   },
 ];
 
+const IBMPlexSans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  style: ["normal", "italic"],
+});
+
 export function SixthSection() {
   return (
-    <div className="sixth-section">
-      {/*
-          Leaders Behind the Vision
-        */}
-      {/* TODO: Implement Carousel Version - Check CUCAI 2024 Repo for more */}
-      <div className="team-title">
-        <p className="small-text">MEET OUR TEAM</p>
-        <h1 className="big-text">Leaders Behind the Vision</h1>
+    <div>
+      <div className="sixth-section">
+        {/*
+            Leaders Behind the Vision
+          */}
+        <div className="team-title">
+          <p className="small-text">MEET OUR TEAM</p>
+          <h1 className="big-text">Leaders Behind the Vision</h1>
+        </div>
       </div>
-
+    {/* Main has some trailing bottom space - delete this */}
+    <main 
+      className={`${IBMPlexSans.className} h-[350px] w-[100dvw] overflow-x-hidden overflow-y-scroll flex flex-col items-center z-10 no-scrollbar font-light text-white pb-32`}
+      style={{ backgroundColor: 'rgba(77, 136, 255, 1)' }}
+    >
       <div
         id="Team"
         className="flex flex-row divide-x-[1px] divide-y-[1px] sm:divide-y-[0px divide-[#55E0FF]/50"
@@ -130,7 +143,71 @@ export function SixthSection() {
             </div>
           ))}
         </div>
+        <div className="carousel-inner flex flex-row items-center h-fit overflow-scroll no-scrollbar divide-x-[1px] divide-[#55E0FF]/50">
+          {Team.map((person, i) => (
+            <div
+              className="flex flex-col justify-center items-center py-8 px-16 gap-4 min-w-[320px]"
+              key={i}
+            >
+              <img
+                src={person.headshot}
+                alt={`Headshot of ${person.name}`}
+                className="h-[150px] rounded-full border-[#55E0FF]/50 border-[1px] object-cover bg-gradient-to-t from-[#55E0FF]/50 to-[#55E0FF]/0"
+              />
+              <div className="text-center">
+                <p
+                  className={`${
+                    person.name.length > 13 ? `!text-[13pt]` : "text-2xl"
+                  } font-normal`}
+                >
+                  {person.name}
+                </p>
+                <p className="text-sm">{person.title}</p>
+              </div>
+              <a
+                target="_blank"
+                className="flex w-full items-center justify-center hover:bg-white/10 rounded-md p-2"
+                href={person.linkedin}
+              >
+                <img src="/linkedin.png" className="h-5" />
+              </a>
+            </div>
+          ))}
+        </div>
+        <div className="carousel-inner flex flex-row items-center h-fit overflow-scroll no-scrollbar divide-x-[1px] divide-[#55E0FF]/50">
+          {Team.map((person, i) => (
+            <div
+              className="flex flex-col justify-center items-center py-8 px-16 gap-4 min-w-[320px]"
+              key={i}
+            >
+              <img
+                src={person.headshot}
+                alt={`Headshot of ${person.name}`}
+                className="h-[150px] rounded-full border-[#55E0FF]/50 border-[1px] object-cover bg-gradient-to-t from-[#55E0FF]/50 to-[#55E0FF]/0"
+              />
+              <div className="text-center">
+                <p
+                  className={`${
+                    person.name.length > 13 ? `!text-[13pt]` : "text-2xl"
+                  } font-normal`}
+                >
+                  {person.name}
+                </p>
+                <p className="text-sm">{person.title}</p>
+              </div>
+              <a
+                target="_blank"
+                className="flex w-full items-center justify-center hover:bg-white/10 rounded-md p-2"
+                href={person.linkedin}
+              >
+                <img src="/linkedin.png" className="h-5" />
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
+    </main>
+    
     </div>
   );
 }
