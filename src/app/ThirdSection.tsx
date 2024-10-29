424a17dc6040db65d0b377abefd53ff7e10e5be6
Kevin Wang <kayhne999@gmail.com>
2024-10-29 19:18:50 -0400

    v0 speaker image and name fix

--- src/app/ThirdSection.tsx
+++ src/app/ThirdSection.tsx
@@ -15,23 +15,20 @@ export default function ThirdSection() {
         </div>
         <div className="speaker-list">
           {speakers.map((speaker) => (
-            <div className="speaker-elem" key={speaker.name}>
-              <Image
+            <div className={"speaker-elem"} key={speaker.name}>
+              {/* <Image
                 src={speaker.img}
                 alt={speaker.name}
                 width={250}
                 height={250}
                 className="speaker-img"
-              />
-              <p className="speaker-name">{speaker.name}</p>
+              /> */}
+              <img src={speaker.img} alt={"Headshot of " + speaker.name} class="h-[250px] rounded-full border-[#002b5c]/50 border-[1px] object-cover bg-gradient-to-t from-[#547192]/0 to-[#002b5c]/50"></img>
+              <p className={"speaker-name" + (speaker.name.length > 15 ? "-long" : "")}>{speaker.name}</p>
               <p className="speaker-text">{speaker.position}</p>
-              <Image
-                src={speaker.company}
-                alt={speaker.company}
-                width={speaker.width}
-                height={55}
-                className="speaker-logo"
-              />
+              <div className="speaker-img">
+                <img src={speaker.company} class="w-[200px] h-[55px] object-contain"></img>
+              </div>
             </div>
           ))}
         </div>
