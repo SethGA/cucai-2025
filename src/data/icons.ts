81a513fd57fcb461450d0aae95c7dca774f915ea
Ryan Su <115808735+ryan-su-12@users.noreply.github.com>
2024-11-24 18:16:12 -0500

    more changes for deployment

---
+++ src/data/icons.ts
@@ -0,0 +1,6 @@
+// TODO: Add CUCAI links here
+export const icons = [
+    { src: "/email.png", link: "/" },
+    { src: "/instagram.png", link: "/" },
+    { src: "/linkedin.png", link: "/" },
+  ];
