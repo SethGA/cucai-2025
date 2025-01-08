f972a5cdd3142f4c73290c8c47549da90a589807
aiejvn <139830381+aiejvn@users.noreply.github.com>
2025-01-07 22:50:46 -0500

    Mobile navigation "fix"
    To be improved upon later - after responsiveness is improved

--- src/components/main-menu.tsx
+++ src/components/main-menu.tsx
@@ -19,38 +19,47 @@ export default function MainMenu({
     font:NextFont;
 }){
     const [isOpen, setIsOpen] = useState(false);
-    const [activeURL, setActiveUrl] = useState("/");
+    const [activeURL, setActiveUrl] = useState("");
 
     // const isDesktop = useMediaQuery("(min-width: 906px)");
     const fontSize = 50;
 
     // const pathname = usePathname();
 
-    useEffect(() => {
-        console.log(window.location.href);
-        console.log(activeURL);
+    // useEffect(() => {
+    //     console.log(window.location.href);
+    //     console.log(activeURL);
 
-        const element = activeURL != "/" && document.querySelector(activeURL);
-        if (!isOpen) {
-            // setActiveUrl(window.location.href);
-            if(element){
-                element.scrollIntoView({block : "start"});
-            }
-        }
-    }, [activeURL, isOpen]);
-    
-    
+    //     const element = activeURL != "/" && document.querySelector(activeURL);
+    //     if (!isOpen) {
+    //         // setActiveUrl(window.location.href);
+    //         if(element){
+    //             element.scrollIntoView({block : "start"});
+    //         }
+    //     }
+    // }, [activeURL, isOpen]);
 
+    
+    // get a neater solution when we have time - for now, just take the user there
+    // page is rerendering when we exit (can be seen by difference in dimensions when we exit)
+    // before: 518.4 X 16845.5
+    // after: 518.2 X 16845.4
+    const jumpPaths = (id:string) => {
+        setIsOpen(false); // to close the drawer (and act like this is normal)
+        window.location.hash = id;
+        window.location.reload();
+    }
 
     return <div className="absolute right-0 pr-4">
-        <Drawer direction="right" open={isOpen} onClose={() => setIsOpen(false)}>
+        <Drawer 
+            direction="right" 
+            onOpenChange={() => {setIsOpen(!isOpen)}}
+            open={isOpen}
+            preventScrollRestoration>
             <DrawerTrigger>
-                <MenuIcon size={75} color="white" className={!isDark ? "invert" : ""} onClick={() => setIsOpen(true)}/>
+                <MenuIcon size={75} color="white" className={!isDark ? "invert" : ""}/>
             </DrawerTrigger>
             <DrawerContent 
-                // style={{
-                //     background: "linear-gradient(rgba(115, 100, 255, 1), rgba(162,216,235, 1))"
-                // }}
                 style = {isDark ? {
                     background: "linear-gradient(180deg, #171456 0%, #6A6CB3)"
                 } : {
@@ -63,7 +72,8 @@ export default function MainMenu({
                         href={b.link} 
                         className={"table-cell p-[10px] " + `text-[${fontSize}px] ` + (isDark ? "invert " : "") + font.className} 
                         key={b.name}
-                        onClick={() => setActiveUrl(b.link)}>
+                        onClick={() => jumpPaths(b.link)}
+                        >
                         {b.name}
                     </a>
                 ))}
