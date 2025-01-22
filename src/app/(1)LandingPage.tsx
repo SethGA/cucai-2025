c1ea5cb17e1da2eee1c6c7ede32e00977b661400
Ryan Su <115808735+ryan-su-12@users.noreply.github.com>
2025-01-22 17:16:20 -0500

    Update (1)LandingPage.tsx
    changed the formatting

--- src/app/(1)LandingPage.tsx
+++ src/app/(1)LandingPage.tsx
@@ -33,57 +33,37 @@ export function Landing({
 
         {/* <Header isDark={isDark} setDark={setDark} /> */}
 
-        <div className="white-box flex flex-col items-center gap-8 w-full sm:hover:scale-110 transition-all">
-          <Countdown font={PixelifySans} isDark={isDark} />
-          <h1 className={"title text-[9vw] max-w-[89vw] break-words sm:text-[84px] " + PixelifySans.className}>
-              Experience AI&apos;s Future at CUCAI 2025!
-          </h1>          
-          
-          {/* {windowWidth >= 706 ? (
-            <h1
-              className={
-                "title text-[50px] sm:text-[84px] " + PixelifySans.className
-              }
-            >
-              Experience AI&apos;s Future at CUCAI 2025!
+        <div className="flex flex-col items-center gap-8 w-full sm:hover:scale-110 transition-all">
+
+          <div className="text-center pt-[10%]">
+            <h1 className={`text-[9vw] leading-tight max-w-[89vw] sm:text-[48px] ${PixelifySans.className} ${isDark ? 'text-white' : 'text-[#171456]'}`}>
+              March 8-9, 2024 | Toronto, ON
             </h1>
-          ) : (
-            <div>
-              <h1
-                className={
-                  "title text-[50px] sm:text-[84px] " + PixelifySans.className
-                }
-              >
-                Experience AI&apos;s
-              </h1>
-              <h1
-                className={
-                  "title text-[50px] sm:text-[84px] " + PixelifySans.className
-                }
-              >
-                Future at
-              </h1>
-              <h1
-                className={
-                  "title text-[50px] sm:text-[84px] " + PixelifySans.className
-                }
-              >
-                CUCAI 2025!
-              </h1>
-            </div>
-          )} */}
-          <a href="https://discord.gg/az6MVvNhdB">
-            <img
-              src="join-discord.png"
-              alt="Apply to CUCAI"
-              className=" h-[50px] w-[152px] hover:brightness-110 active:brightness-90"
+            <h1 className={`title text-[9vw] leading-tight max-w-[89vw] sm:text-[148px] ${PixelifySans.className} ${isDark ? 'text-white' : 'text-[#171456]'}`}>
+              CUCAI 2025!
+            </h1>
+            <Countdown 
+              font={PixelifySans} 
+              isDark={isDark} 
+              size={`text-[24px] sm:text-[36px] ${isDark ? 'text-white' : 'text-[#171456]'}`} 
             />
-          </a>
+          </div>
+
         </div>
       </div>
     </div>
   );
 }
+
+/*
+Fixes:
+ landing page Toronto backdrop 
+- 1. fix so that it covers entire screen (left and right edges aren't covered)
+- 2. remove thin blue line between Toronto backdrop and starting fade of section 2 (about cucai)
+
+- 1. Fixed using background-size: cover
+- 2. png has the thin line, adjust the png and we'll be ok
+*/
 
 /*
 Fixes:
